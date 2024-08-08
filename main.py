b2f5b3739a339e53018357a618ee7a96c160f22a
Jan Borowiec <janekborowiec@gmail.com>
2024-08-08 16:35:29 +0200

    Centralized code running into main

---
+++ main.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from typing import Any
+
+import inquirer
+
+from runners.run_file_generation import run_random_file_generation
+
+
+class ProgramAction(Enum):
+    run_program = "Run Program"
+    run_tests = "Run Tests"
+    generate_random_files = "Generate Random Files"
+
+    @classmethod
+    def list(cls) -> list[Any]:
+        return list(map(lambda c: c.value, cls))
+
+
+if __name__ == "__main__":
+    questions = [
+        inquirer.List(
+            "action",
+            message="Select action:",
+            choices=ProgramAction.list(),
+        ),
+    ]
+    answers = inquirer.prompt(questions)
+
+    match answers["action"]:
+        case ProgramAction.run_program.value:
+            print("Run P")
+        case ProgramAction.run_tests.value:
+            print("Run t")
+        case ProgramAction.generate_random_files.value:
+            run_random_file_generation()
